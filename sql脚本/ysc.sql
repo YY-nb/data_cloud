8bc687e95f019bec527437f2e5759096183dc8c3
YY-nb <916295483@qq.com>
2021-06-05 21:16:05 +0800

    完善数据库表

--- sql脚本/ysc.sql
+++ sql脚本/ysc.sql
@@ -22,33 +22,33 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `folder`;
 CREATE TABLE `folder`  (
-            `id` char(32) NOT NULL,
-           `pid` char(32) DEFAULT NULL,
-           `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '文件或文件夹名称',
-           `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '文件类型',
-           `file_id` int DEFAULT NULL COMMENT '文件id  空表示是文件夹',
-           `file_stor_id` char(32) DEFAULT NULL COMMENT '创建人',
-           `create_time` datetime DEFAULT NULL,
-           `update_time` datetime DEFAULT NULL,
-           PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+                           `id` char(32) NOT NULL,
+                           `pid` char(32) DEFAULT NULL,
+                           `name` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '文件或文件夹名称',
+                           `type` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '文件类型',
+                           `file_id` int DEFAULT NULL COMMENT '文件id  空表示是文件夹',
+                           `file_stor_id` char(32) DEFAULT NULL COMMENT '创建人',
+                           `create_time` datetime DEFAULT NULL,
+                           `update_time` datetime DEFAULT NULL,
+                           PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_file
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_file`;
 CREATE TABLE `sys_file`  (
-             `id` char(32) NOT NULL,
-             `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '文件名称',
-             `path` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '文件相对路径',
-             `file_store_id` char(32) DEFAULT NULL COMMENT '创建人',
-             `create_time` datetime DEFAULT NULL,
-             `update_time` datetime DEFAULT NULL,
-             `pid` char(32) DEFAULT NULL,
-             `size` double DEFAULT NULL,
-             `type` varchar(100) DEFAULT NULL,
-             PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+                             `id` char(32) NOT NULL,
+                             `name` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '文件名称',
+                             `path` varchar(1024) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '文件相对路径',
+                             `file_store_id` char(32) DEFAULT NULL COMMENT '创建人',
+                             `create_time` datetime DEFAULT NULL,
+                             `update_time` datetime DEFAULT NULL,
+                             `pid` char(32) DEFAULT NULL,
+                             `size` double DEFAULT NULL,
+                             `type` varchar(100) DEFAULT NULL,
+                             PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4  ROW_FORMAT = Dynamic;
 
 
 
@@ -57,12 +57,12 @@ CREATE TABLE `sys_file`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
 CREATE TABLE `sys_role`  (
-             `role_id` char(32) NOT NULL,
-             `role_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '角色名称',
-             `user_id` char(32) DEFAULT NULL COMMENT '创建者ID',
-             `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-             PRIMARY KEY (`role_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色' ROW_FORMAT = Dynamic;
+                             `role_id` char(32) NOT NULL,
+                             `role_name` varchar(100) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '角色名称',
+                             `user_id` char(32) DEFAULT NULL COMMENT '创建者ID',
+                             `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                             PRIMARY KEY (`role_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4  COMMENT = '角色' ROW_FORMAT = Dynamic;
 
 
 -- ----------------------------
@@ -70,27 +70,27 @@ CREATE TABLE `sys_role`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user`  (
-             `user_id` char(32) NOT NULL AUTO_INCREMENT,
-             `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
-             `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
-             `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
-             `mobile` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '手机号',
-             `status` tinyint(0) NULL DEFAULT NULL COMMENT '状态  0：禁用   1：正常',
-             `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-             `file_store_id` char(32) DEFAULT NULL,
-             PRIMARY KEY (`user_id`) USING BTREE,
-             UNIQUE INDEX `username`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '系统用户' ROW_FORMAT = Dynamic;
+                             `user_id` char(32) NOT NULL ,
+                             `username` varchar(50) CHARACTER SET utf8mb4  NOT NULL COMMENT '用户名',
+                             `password` varchar(100) CHARACTER SET utf8mb4  NULL DEFAULT NULL COMMENT '密码',
+                             `email` varchar(100) CHARACTER SET utf8mb4  NULL DEFAULT NULL COMMENT '邮箱',
+                             `mobile` varchar(100) CHARACTER SET utf8mb4  NULL DEFAULT NULL COMMENT '手机号',
+                             `status` tinyint(0) NULL DEFAULT NULL COMMENT '状态  0：禁用   1：正常',
+                             `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+                             `file_store_id` char(32) DEFAULT NULL,
+                             PRIMARY KEY (`user_id`) USING BTREE,
+                             UNIQUE INDEX `username`(`username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4  COMMENT = '系统用户' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
-         `id` char(32) NOT NULL,
-         `user_id` char(32) DEFAULT NULL,
-         `role_id` char(32) DEFAULT NULL,
-         PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户角色关系' ROW_FORMAT = Dynamic;
+                             `id` char(32) NOT NULL,
+                             `user_id` char(32) DEFAULT NULL,
+                             `role_id` char(32) DEFAULT NULL,
+                             PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4  COMMENT = '用户角色关系' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
