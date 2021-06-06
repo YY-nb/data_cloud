a590536ce481a57ed57e161a93efc62b800aba4b
YY-nb <916295483@qq.com>
2021-06-06 10:52:18 +0800

    解决跨域

--- data_cloud-server/src/main/java/com/project/data_cloud_server/config/web/WebMvcConfig.java
+++ data_cloud-server/src/main/java/com/project/data_cloud_server/config/web/WebMvcConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("")
-                .allowedHeaders("")
-                .allowedMethods("")
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
                 .maxAge(1800)
-                .allowedOrigins("");
+                .allowedOrigins("*");
     }
 }
